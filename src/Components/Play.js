ca4b45d13d1942203898a3a4b67609e33a236c67
Alpha Keita <78216570+alphakta@users.noreply.github.com>
2021-12-15 13:11:37 +0100

    maj

--- src/Components/Play.js
+++ src/Components/Play.js
@@ -3,31 +3,61 @@ import Word from './Word';
 
 export default function Play(props) {
 
-    const [word, setWord] = useState(props.word)
-    const [usedLetter, setTabLetter] = useState(word.charAt(0))
+    const [word] = useState(props.word.toLowerCase())
+    const [usedLetter, setTabLetter] = useState([word.charAt(0)])
+    const [score, setScore] = useState(10)
 
-    // const [score] = useState(0)
+    function verifWin() {
+        let wordsHidden = document.getElementsByClassName("wordHidden")
+        let count = wordsHidden.length
+        for (let i = 0; i < wordsHidden.length; i++) {
+            let letterR = wordsHidden[i].textContent
 
-    console.log(word)
+            if (letterR.includes('-'))
+                 count--
 
-    // setWord(word + '1')
+        }
+        console.log(count)
 
-    // setTabLetter(usedLetter = [].push(word.charAt(0)))
+        if(count === wordsHidden.length-1 ||count >= wordsHidden.length-1)
+            return true
+        else
+            return false
+    }
+    function handleLetterPress(e) {
+        setTabLetter(usedLetter.concat(e.key))
+        setScore(score - 1)
+    }
 
-	// function handleLetterPress(e){
-    //     setTabLetter(usedLetter.push(e.key))
-    // }
-
-
-    // console.log(state)
-    if(word == undefined)
+    if (word === undefined)
         return <p>waiting</p>
+
+    if (score === 0) {
+        return (
+            <div>
+                <Word wordUsed={word} usedLetter={usedLetter} />
+                <p> Write your letters</p>
+                <p> The test number has been used, it's lose !</p>
+            </div>
+        )
+    }
+
+    if(verifWin())
+        return (
+            <div>
+            <Word wordUsed={word} usedLetter={usedLetter} />
+            <p> Write your letters</p>
+            <p> It's win !</p>
+        </div>
+        )
 
     return (
         <div>
-            <Word wordUsed={word} usedLetter={usedLetter}/>
+            <Word wordUsed={word} usedLetter={usedLetter} />
+            <p> Vous avez {score} essai(s) </p>
+            <p> Voici les lettes essayer : {usedLetter + ','}</p>
             <p> Write your letters</p>
-			{/* <input type="text"  onKeyPress={handleLetterPress}/> */}
+            <input type="text" onKeyPress={handleLetterPress} />
         </div>
     )
 }
