476f33c4135a021fcf4e04dfcee9d694f2370842
Alpha Keita <78216570+alphakta@users.noreply.github.com>
2021-12-15 21:28:44 +0100

    commit maj

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { ThemeContext } from './contexts/theme';
  
 
 function App() {
-  const [username, setUsername] = useState(localStorage.getItem("username") || undefined)
+  const [username] = useState(localStorage.getItem("username") || undefined)
   const [{theme, isDark}, toggleTheme] = useContext(ThemeContext)
   const [word, setWord] = useState(undefined)
 
@@ -24,12 +24,21 @@ function App() {
     return await dataWord.json()
   };
 
+  const deconnecter = () => {
+    localStorage.removeItem('username')
+    refreshPage()
+  }
+
+  function refreshPage(){ 
+    window.location.reload(); 
+  }
   if(!word)
     return <p> waiting </p>
 
-  if(username == undefined)
+  if(username === undefined)
     return(
       <div className="App" style={{backgroundColor: theme.backgroundColor, color: theme.color, position: 'relative' }}>
+      <button className="toggleButton" onClick={toggleTheme}> {isDark ? "Sombre" : "Clair"}</button>
       <h1>Animal Finder</h1>
       <p>Trouve l'animal pour gagné des points</p>
         <Login username={username}/>
@@ -39,6 +48,7 @@ function App() {
   return (
     <div className="App" style={{backgroundColor: theme.backgroundColor, color: theme.color, position: 'relative' }}>
       <button className="toggleButton" onClick={toggleTheme}> {isDark ? "Sombre" : "Clair"}</button>
+      <button className="deconecteSession" onClick={deconnecter}> Se déconnecter </button>
       <h1>Animal Finder</h1>
       <p>Trouve l'animal pour gagné des points</p>
       <Play word={word.word}/>
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,22 +1,42 @@
 import React from 'react'
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './css/login.css'
 
 
 export default function Login() {
     const [username, setUsername] = useState('');
-  
 
-      function handlePress(e){
+      const getUser = async () => {
+        const dataUser  = await fetch(`https://animalfinderapi.herokuapp.com/user/${username}`)
+        return await dataUser.json();
+      };
+
+    function onChange(e) {
         setUsername(e.target.value)
-      }
+    }
     
+    const pressEnregistrer = async () => {
+        const users = await getUser();
+        console.log(users)
 
-      console.log(username);
+        if (users.data) {
+            if(window.confirm("L'utilisateur existe déjà, veux-tu continuer ?"))
+                localStorage.setItem('username', username);
+        } else  
+            localStorage.setItem('username', username);
+        
+        refreshPage()
+    }
+
+    function refreshPage(){ 
+        window.location.reload(); 
+    }
+
+    console.log(username);
     return (
         <div className='centerLogin'>
-            <input placeholder='Choisis ton pseudo !' value={username} onChange={handlePress} ></input>
-            <button > S'enregistrer </button>
+            <input placeholder='Choisis ton pseudo !' value={username} onChange={onChange} ></input>
+            <button onClick={pressEnregistrer}> S'enregistrer </button>
         </div>
     )
 }
